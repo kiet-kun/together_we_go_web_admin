192983f0117d6b01ca4b117a793985487dd1ac0b
kiet <79886528+kiet-kun@users.noreply.github.com>
2023-09-14 16:25:56 +0700

    Add bootstap, theme mode, sign up page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import LoginPage from './pages/auth/login/index';
+// import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import { THEME, initTheme, toggleTheme, setTheme, resetTheme } from './Theme'
+
+initTheme()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <LoginPage/> 
   </React.StrictMode>
 );
 
