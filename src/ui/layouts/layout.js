8dae685ff633a5c80f62673dfcd2c1cfea4a13a4
kietdev123 <79886528+kietdev123@users.noreply.github.com>
2024-04-25 23:02:07 +0700

    styles: fix side bar

--- src/ui/layouts/layout.js
+++ src/ui/layouts/layout.js
@@ -7,53 +7,17 @@ import { setTheme, THEME } from '@/utils/theme.js';
 const Layout = ({ children }) => {
   return (
     <>
-    <div>
-      <div class="d-flex justify-content-between">
-          <div style={{width: '20vw'}}>
-            <SideBar></SideBar> 
-          </div>         
-          <div style={{width: '80vw', paddingTop: '5vh', paddingRight: '8px'}}>        
+      <div>
+        <div class="d-flex">
+          <SideBar />
+          <div class="vh-100 vw-100 overflow-auto">
+            <div className="mx-5 pt-5">
               {children}
+            </div>
+            <LayoutFooter />
           </div>
+        </div>
       </div>
-      
-      <div style={{width: '100%', height:'100px'}}>
-          <LayoutFooter></LayoutFooter>
-      </div>
-      
-       {/* Change dark/light mode buttom */}
-       {/* <div class="dropdown position-fixed bottom-0 end-0 mb-3 me-3 bd-mode-toggle">
-          <button class="btn btn-bd-primary py-2 dropdown-toggle d-flex align-items-center"
-                  id="bd-theme"
-                  type="button"
-                  aria-expanded="false"
-                  data-bs-toggle="dropdown"
-                  aria-label="Toggle theme (auto)">
-            <i class="bi bi-circle-half me-2 opacity-50 theme-icon"  width="1em" height="1em"></i>            
-          </button>
-          <ul class="dropdown-menu dropdown-menu-end shadow" aria-labelledby="bd-theme-text">
-            <li>
-              <button  onClick={() => setTheme(THEME.LIGHT)} type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="light" aria-pressed="false">
-              <i class="bi bi-lightbulb me-2 opacity-50 theme-icon" width="1em" height="1em"></i>
-                Sáng
-              </button>
-            </li>
-            <li>
-              <button  onClick={() => setTheme(THEME.DARK)} type="button" class="dropdown-item d-flex align-items-center" data-bs-theme-value="dark" aria-pressed="false">
-                <i class="bi bi-moon-fill me-2 opacity-50 theme-icon"  width="1em" height="1em"></i>
-                Tối
-              </button>
-            </li>
-            <li>
-              <button type="button" class="dropdown-item d-flex align-items-center active" data-bs-theme-value="auto" aria-pressed="true">
-                <i class="bi bi-circle-half me-2 opacity-50 theme-icon"  width="1em" height="1em"></i>
-                Tự độ
-              </button>
-            </li>
-          </ul>
-      </div> */}
-    </div>
-  
     </>
   )
 }
--- src/ui/layouts/sidebar/index.js
+++ src/ui/layouts/sidebar/index.js
@@ -1,105 +1,135 @@
 import { PAGE_NAME } from '@/constanst';
 import './index.css';
 import { NavLink } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from "react"
+
+const menuItem = [
+    {
+        path: PAGE_NAME.home,
+        name: "Trang chủ",
+        icon: "bi-house-door"
+    },
+    {
+        path: "#",
+        name: "Thống kê",
+        icon: "bi-speedometer2"
+    },
+    {
+        path: PAGE_NAME.user,
+        name: "Tài khoản",
+        icon: "bi-person"
+    },
+    {
+        path: PAGE_NAME.booking,
+        name: "Chuyến đi",
+        icon: "bi-postcard"
+    },
+]
 
 const SideBar = () => {
-    const menuItem=[
-        {
-            path:"/trang-chu",
-            name:"Trang chủ",
-            icon:<i class="bi bi-house-door pe-none me-2" width="16" height="16"></i>
-        },
-        {
-            path:"#",
-            name:"Thống kê",
-            icon:<i class="bi bi-speedometer2 pe-none me-2" width="16" height="16"></i>
-        },
-        {
-            path: PAGE_NAME.user,
-            name:"Tài khoản",
-            icon:<i class="bi bi-person pe-none me-2" width="16" height="16"></i>
-        },
-        {
-            path: PAGE_NAME.booking,
-            name:"Chuyến đi",
-            icon:<i class="bi bi-postcard pe-none me-2" width="16" height="16"></i>
-        },
-    ]
+    const location = useLocation()
+    const currentPage = location.pathname
+    const [isSmall, setIsSmall] = useState(false)
 
-    return  <>
-         <div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" style={{width: '240px', height: '100vh'}} >
-            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-            {/* <svg class="bi pe-none me-2" width="40" height="32"><use xlink:href="#bootstrap"/></svg> */}
-            <img class="bi pe-none me-2" width="40" height="32" 
-            src="/assets/image/twg_logo.png" alt="logo" />
-            <span class="fs-4" style={{fontWeight : 'bold', color: 'grey'}}>TWG Admin</span>
+    const handleClick = () => {
+        setIsSmall(!isSmall)
+    }
+
+    return <>
+    {!isSmall && <div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" 
+            style={{ width: '240px', height: '100vh' }} >
+            <a 
+            onClick={handleClick}
+            href="#" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+               
+                <img class="bi pe-none me-2" width="40" height="32"
+                    src="/assets/image/twg_logo.png" alt="logo" />
+                    <span class="fs-4" style={{ fontWeight: 'bold', color: 'grey' }}>TWG Admin</span>
+                  
             </a>
             <hr></hr>
             <ul class="nav nav-pills flex-column mb-auto">
-            {/* <li class="nav-item">
-                <a href="/trang-chu" class="nav-link link-body-emphasis" aria-current="page">
-                <i class="bi bi-house-door pe-none me-2" width="16" height="16"></i>
-               
-                Trang chủ
-                </a>
-            </li>
-            <li>
-                <a href="#" class="nav-link link-body-emphasis">
-                <i class="bi bi-speedometer2 pe-none me-2" width="16" height="16"></i>
-                
-                Thống kê
-                </a>
-            </li>
-            <li>
-                <a href="/quan-ly-nguoi-dung" class="nav-link link-body-emphasis">
-                <i class="bi bi-person pe-none me-2" width="16" height="16"></i>
-               
-                Tài khoản
-                </a>
-            </li>
-            <li>
-                <a href="#" class="nav-link link-body-emphasis">
-                <i class="bi bi-postcard pe-none me-2" width="16" height="16"></i>
-                
-                Chuyến đi
-                </a>
-            </li> */}
-             {
-                   menuItem.map((item, index)=>(
-                    //    <NavLink to={item.path} key={index} className="link" activeclassName="active">
-                    //        <div className="icon">{item.icon}</div>
-                    //        <div style={{display: isOpen ? "block" : "none"}} className="link_text">{item.name}</div>
-                    //    </NavLink>
-                    //    <NavLink to={item.path} className="nav-link link" activeClassName="active">
-                 
-                    //         {item.icon}      
-                    //         {item.name}
-                      
-                    //     </NavLink>
-                         <li>
-                         <a href={item.path} class="nav-link link"  key={index} activeclassName="active">
-                            {item.icon}      
-                            {item.name}
-                         </a>
-                     </li>
-                   ))
-            }
+
+                {
+                    menuItem.map((item, index) => (
+
+                        <li>
+                            <a href={item.path}
+                                class={`nav-link   ${(item.path == currentPage) ? "active" : "link-body-emphasis"}`}
+                                key={index}>
+                       
+                                <i class={`bi ${item.icon} pe-none me-2`} width="16" height="16"></i>
+                                {item.name}                               
+                            </a>
+                        </li>
+
+                    ))
+                }
             </ul>
-           
+
             <hr></hr>
             <div class="dropdown">
-            <a href="#" class="d-flex align-items-center link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2"/>
-                <strong>Kiet Nguyen</strong>
-            </a>
-            <ul class="dropdown-menu text-small shadow">
-                <li><a class="dropdown-item" href="#">Cài đặt</a></li>
-                <li><a class="dropdown-item" href="#">Thông tin cá nhân</a></li>
-                <li><hr class="dropdown-divider"/></li>
-                <li><a class="dropdown-item" href="/dang-nhap">Đăng xuất</a></li>
-            </ul>
+                <a href="#" class="d-flex align-items-center link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2" />
+                    <strong>Kiet Nguyen</strong>
+                </a>
+                <ul class="dropdown-menu text-small shadow">
+                    <li><a class="dropdown-item" href="#">Cài đặt</a></li>
+                    <li><a class="dropdown-item" href="#">Thông tin cá nhân</a></li>
+                    <li><hr class="dropdown-divider" /></li>
+                    <li><a class="dropdown-item" href="/dang-nhap">Đăng xuất</a></li>
+                </ul>
             </div>
         </div>
+    }
+         {isSmall &&
+
+        <div class="d-flex flex-column flex-shrink-0 bg-body-tertiary" style={{width: '4.5rem'}}>
+    <a 
+     onClick={handleClick}
+    href="#" class="d-block p-3 link-body-emphasis text-decoration-none" title="Icon-only" data-bs-toggle="tooltip" data-bs-placement="right">
+      <img class="bi pe-none me-2" width="40" height="32"
+                    src="/assets/image/twg_logo.png" alt="logo" />
+                     
+      <span class="visually-hidden">Icon-only</span>
+    </a>
+    <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
+
+    {
+                    menuItem.map((item, index) => (
+
+                        <li>
+                            <a href={item.path}
+                                title={item.name}
+                                class={`nav-link  py-3 border-bottom rounded-0 ${(item.path == currentPage) ? "active" : ""}`}
+                                key={index}>
+                                
+                                <i class={`bi ${item.icon} pe-none`} width="24" height="24"></i>
+                                
+                               
+                            </a>
+                            
+                        </li>
+
+                    ))
+                }
+
+    </ul>
+    <div class="dropdown border-top">
+      <a href="#" class="d-flex align-items-center justify-content-center p-3 link-body-emphasis text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+      <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2" />
+      </a>
+      <ul class="dropdown-menu text-small shadow">
+        <li><a class="dropdown-item" href="#">New project...</a></li>
+        <li><a class="dropdown-item" href="#">Settings</a></li>
+        <li><a class="dropdown-item" href="#">Profile</a></li>
+        <li><hr class="dropdown-divider"/></li>
+        <li><a class="dropdown-item" href="#">Sign out</a></li>
+      </ul>
+    </div>
+  </div> }
+
     </>
 }
 
